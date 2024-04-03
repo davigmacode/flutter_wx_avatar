c766e8e485c0e38046b412fa7a3e8be4ef4c4bce
Irfan Vigma Taufik <davigmacode@gmail.com>
2024-04-03 23:12:40 +0700

    Improved example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wx_text/wx_text.dart';
+import 'package:wx_avatar/wx_avatar.dart';
+import 'package:overlap_stack/overlap_stack.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +10,156 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WxAvatar Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const SizedBox(height: 40),
+            const WxText.displayMedium('WxAvatar'),
+            const SizedBox(height: 40),
+            Wrapper(
+              title: 'Rectangle Shape',
+              child: Wrap(
+                spacing: 20,
+                children: [
+                  const WxAvatar(
+                    image: NetworkImage('https://i.pravatar.cc/50?u=2'),
+                  ),
+                  const WxAvatar(
+                    elevation: 3.0,
+                    backgroundColor: Colors.red,
+                    foregroundStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amber,
+                    ),
+                    child: Text('Wx'),
+                  ),
+                  WxAvatar(
+                    borderStyle: BorderStyle.solid,
+                    borderColor: Colors.black54,
+                    borderWidth: 2,
+                    backgroundColor: Theme.of(context).colorScheme.surface,
+                    foregroundColor: Colors.black87,
+                    foregroundSize: 40,
+                    child: const Icon(Icons.person),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 20),
+            Wrapper(
+              title: 'Circle Shape',
+              child: Wrap(
+                spacing: 20,
+                children: [
+                  const WxAvatar.circle(
+                    image: NetworkImage('https://i.pravatar.cc/50?u=2'),
+                  ),
+                  const WxAvatar.circle(
+                    elevation: 3.0,
+                    backgroundColor: Colors.red,
+                    child: Text('Wx'),
+                  ),
+                  WxAvatar.circle(
+                    borderStyle: BorderStyle.solid,
+                    borderColor: Colors.black54,
+                    borderWidth: 2,
+                    backgroundColor: Theme.of(context).colorScheme.surface,
+                    foregroundColor: Colors.black87,
+                    foregroundSize: 40,
+                    child: const Icon(Icons.person),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Wrapper(
+              title: 'Grouped Avatar',
+              child: OverlapStack.builder(
+                minSpacing: 0.5,
+                maxSpacing: 0.8,
+                leadIndex: 3,
+                itemSize: const Size.square(30),
+                itemLimit: 7,
+                itemCount: 25,
+                itemBuilder: (context, i) {
+                  return WxAvatar.circle(
+                    radius: 15,
+                    borderWidth: 3,
+                    borderStyle: BorderStyle.solid,
+                    borderColor: Theme.of(context).colorScheme.surface,
+                    image: NetworkImage('https://i.pravatar.cc/50?u=$i'),
+                  );
+                },
+                infoBuilder: (context, remaining) {
+                  return WxAvatar.circle(
+                    radius: 15,
+                    elevation: 3.0,
+                    foregroundSize: 11,
+                    backgroundColor: Colors.red,
+                    child: Text('+$remaining'),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  final String title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+          child: WxText.labelLarge(title),
+        ),
+        SizedBox(
+          width: 300,
+          height: 100,
+          child: Card.outlined(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Center(child: child),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
